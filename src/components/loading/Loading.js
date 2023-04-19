9f51819d8fc06690baa2a5943212bc808e55eac3
Raafat-Saleh <111872293+Raafat-Saleh@users.noreply.github.com>
2023-04-19 18:55:10 +0200

    Update Loading.js

--- src/components/loading/Loading.js
+++ src/components/loading/Loading.js
@@ -6,7 +6,7 @@ import styles from "./Loading.module.css";
 export default function Loading() {
   return (
     <div className={styles.backgroundcolor}>
-      <div className={styles.boxLoading}></div>
+      <div className={styles.boxLoading}></div> 
     </div>
   );
 }
